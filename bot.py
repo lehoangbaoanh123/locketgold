3aff3bb6bedbbba27837e02c923f76c5ad2324f1
lehoangbaoanh123 <141120356+lehoangbaoanh123@users.noreply.github.com>
2026-02-21 01:47:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-BOT_TOKEN = ""
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 waiting_user = {}
 
